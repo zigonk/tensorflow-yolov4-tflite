147f53dafda6c2a6019cd16ea8ef835e6305a5b2
Thuc Nguyen <zthucnguyen99@gmail.com>
2020-08-26 13:11:31 +0700

    hotfix: missing import

--- detectvideo.py
+++ detectvideo.py
@@ -14,6 +14,7 @@ import numpy as np
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 import os
+import json
 
 flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
 flags.DEFINE_string('weights', './checkpoints/yolov4-416',
