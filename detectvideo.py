79eba0ab11a1d969d949f0619f4acee52c5e4378
Thuc Nguyen <zthucnguyen99@gmail.com>
2020-08-28 09:24:37 +0700

    ffmpeg

--- detectvideo.py
+++ detectvideo.py
@@ -1,20 +1,22 @@
+import ffmpeg
+import json
+import os
+import subprocess as sp
+from tensorflow.compat.v1 import InteractiveSession
+from tensorflow.compat.v1 import ConfigProto
+import numpy as np
+import cv2
+from PIL import Image
+from tensorflow.python.saved_model import tag_constants
+from core.yolov4 import filter_boxes
+import core.utils as utils
+from absl.flags import FLAGS
+from absl import app, flags, logging
 import time
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
-from absl import app, flags, logging
-from absl.flags import FLAGS
-import core.utils as utils
-from core.yolov4 import filter_boxes
-from tensorflow.python.saved_model import tag_constants
-from PIL import Image
-import cv2
-import numpy as np
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
-import os
-import json
 
 flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
 flags.DEFINE_string('weights', './checkpoints/yolov4-416',
@@ -28,8 +30,12 @@ flags.DEFINE_float('score', 0.25, 'score threshold')
 flags.DEFINE_string('output', None, 'path to output video')
 flags.DEFINE_string('bbox_path', None, 'path to bbox json')
 flags.DEFINE_boolean('verbose', False, 'Log current frame id')
-flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
-flags.DEFINE_boolean('dis_cv2_window', False, 'disable cv2 window during the process') # this is good for the .ipynb
+flags.DEFINE_string('output_format', 'XVID',
+                    'codec used in VideoWriter when saving video to file')
+# this is good for the .ipynb
+flags.DEFINE_boolean('dis_cv2_window', False,
+                     'disable cv2 window during the process')
+
 
 def export_bbox(image, bboxes):
     out_boxes, out_scores, out_classes, num_boxes = bboxes
@@ -37,7 +43,8 @@ def export_bbox(image, bboxes):
     image_h, image_w, _ = image.shape
     for i in range(num_boxes[0]):
         result = {}
-        if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > 20: continue
+        if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > 20:
+            continue
         coor = out_boxes[0][i]
         coor[0] = int(coor[0])
         coor[2] = int(coor[2])
@@ -53,6 +60,20 @@ def export_bbox(image, bboxes):
     return annotations
 
 
+def convert_video_numpy(file_name):
+    out, _ = (
+        ffmpeg
+        .input('in.mp4')
+        .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+        .run(capture_stdout=True)
+    )
+    video = (
+        np
+        .frombuffer(out, np.uint8)
+    )
+    return video
+
+
 def main(_argv):
     config = ConfigProto()
     config.gpu_options.allow_growth = True
@@ -63,7 +84,7 @@ def main(_argv):
     output_vid = FLAGS.output
     bbox_path = FLAGS.bbox_path
 
-    print("Video from: ", video_path )
+    print("Video from: ", video_path)
     for ind in range(1, 26):
         record = {}
         record['videoID'] = ind
@@ -71,14 +92,21 @@ def main(_argv):
 
         video_id = 'cam_' + str(ind).zfill(2)
         output_bbox = os.path.join(bbox_path, video_id + '.json')
-        output_fname = os.path.join(output_vid, video_id + '.avi')
+        output_fname = os.path.join(output_vid, video_id + '.mp4')
         video_fname = os.path.join(video_path, video_id + '.mp4')
 
         print("Video path:" + video_fname)
         print("Output path:" + output_fname)
         print("Bbox path:" + output_bbox)
-        vid = cv2.VideoCapture(video_fname)
-
+        frames = convert_video_numpy(video_fname)
+        n, height, width, channels = frames.shape
+        process = (
+            ffmpeg
+            .input('pipe:', format='rawvideo', pix_fmt='rgb', s='{}x{}'.format(width, height))
+            .output(output_fname, pix_fmt='yuv420p', vcodec='libx264', r=60)
+            .overwrite_output()
+            .run_async(pipe_stdin=True)
+        )   
         if FLAGS.framework == 'tflite':
             interpreter = tf.lite.Interpreter(model_path=FLAGS.weights)
             interpreter.allocate_tensors()
@@ -87,33 +115,18 @@ def main(_argv):
             print(input_details)
             print(output_details)
         else:
-            saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+            saved_model_loaded = tf.saved_model.load(
+                FLAGS.weights, tags=[tag_constants.SERVING])
             infer = saved_model_loaded.signatures['serving_default']
-        
-        if FLAGS.output:
-            # by default VideoCapture returns float instead of int
-            width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-            height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            fps = int(vid.get(cv2.CAP_PROP_FPS))
-            codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
-            out = cv2.VideoWriter(output_fname, codec, fps, (width, height))
 
         frame_id = 0
         with open(output_bbox, "w") as f:
             f.write('[\n')
-        prev_time = time.time()
-        while True:
+        for frame in frames:
             record['frameID'] = frame_id
-            return_value, frame = vid.read()
-            if return_value:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                image = Image.fromarray(frame)
-            else:
-                if frame_id == vid.get(cv2.CAP_PROP_FRAME_COUNT):
-                    print("Video processing complete")
-                    break
-                raise ValueError("No image! Try with another video format")
-            
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(frame)
+
             frame_size = frame.shape[:2]
             image_data = cv2.resize(frame, (input_size, input_size))
             image_data = image_data / 255.
@@ -122,7 +135,8 @@ def main(_argv):
             if FLAGS.framework == 'tflite':
                 interpreter.set_tensor(input_details[0]['index'], image_data)
                 interpreter.invoke()
-                pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+                pred = [interpreter.get_tensor(
+                    output_details[i]['index']) for i in range(len(output_details))]
                 if FLAGS.model == 'yolov3' and FLAGS.tiny == True:
                     boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=0.25,
                                                     input_shape=tf.constant([input_size, input_size]))
@@ -145,7 +159,8 @@ def main(_argv):
                 iou_threshold=FLAGS.iou,
                 score_threshold=FLAGS.score
             )
-            pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+            pred_bbox = [boxes.numpy(), scores.numpy(
+            ), classes.numpy(), valid_detections.numpy()]
             image = utils.draw_bbox(frame, pred_bbox)
             record['annotations'] = export_bbox(frame, pred_bbox)
             with open(output_bbox, 'a') as f:
@@ -157,23 +172,30 @@ def main(_argv):
             if not FLAGS.dis_cv2_window:
                 cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
                 cv2.imshow("result", result)
-                if cv2.waitKey(1) & 0xFF == ord('q'): break
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
 
             if FLAGS.output:
-                out.write(result)
+                process.stdin.write(
+                frame
+                    .astype(np.uint8)
+                    .tobytes()
+                )
             if (FLAGS.verbose):
                 print("FrameID: {}".format(frame_id))
                 print("Video ID: {}".format(ind))
-                print("Annotations count: {}".format(len(record['annotations'])))
+                print("Annotations count: {}".format(
+                    len(record['annotations'])))
             frame_id += 1
         curr_time = time.time()
         exec_time = curr_time - prev_time
-        info = "time: %.2f ms" %(1000*exec_time)
+        info = "time: %.2f ms" % (1000*exec_time)
         # print(info)
-        out.release()
-        vid.release()
+        process.stdin.close()
+        process.wait()
         with open(output_bbox, "a") as f:
             f.write(']')
+
 
 if __name__ == '__main__':
     try:
